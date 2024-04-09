dfff168a84e7ce794565e1bad10f8d1634466bec
Nikhil Gupta <nikhilg7404@gmail.com>
2024-04-09 23:32:45 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express")
+const app = express()
+
+// require('dotenv').config()
+
+const bodyparser = require("body-parser")
+app.use(bodyparser.json())
+const PORT = process.env.PORT ||3000
+
+
+
+
+
+
+app.listen(PORT)
